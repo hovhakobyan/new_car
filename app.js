211d1a224afb2681dd7d8e8f394447cde824c147
hovhakobyan <hov.hakobyan2012@gmail.com>
2019-04-06 13:12:23 +0400

    change port

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+console.log('-------------------------------------');
+console.log('-------------------------------------');
+console.log('-------------------------------------');
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
